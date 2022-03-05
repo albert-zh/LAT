ddee0d92a37cb6acefe3d4bb6fc0b70a386cc260
Hongbo <zhaohb94@163.com>
2022-03-05 17:16:27 +0800

    use pass manager to control pass

--- init.cpp
+++ init.cpp
@@ -22,24 +22,24 @@ static ManagedStatic<LLVMContext> GlobalContext;
 // 命令行位置参数全局变量, 这个参数的含义是需要处理的LLVM IR字节码的文件名
 static cl::opt<std::string> InputFilename(cl::Positional, cl::desc("<filename>.bc"), cl::Required);
 
-static cl::opt<bool> EnableCalleeCount("enable-callee-count", cl::Hidden,
-                                        cl::desc("Enable callee count anslysis"),
+static cl::opt<bool> DisableCalleeCount("disable-callee-count", cl::Hidden,
+                                        cl::desc("Disable callee count anslysis"),
                                         cl::init(false));
 
-static cl::opt<bool> EnableBranchCount("enable-branch-count", cl::Hidden,
-                                        cl::desc("Enable callee count anslysis"),
+static cl::opt<bool> DisableBranchCount("disable-branch-count", cl::Hidden,
+                                        cl::desc("Disable callee count anslysis"),
                                         cl::init(false));
 
-static cl::opt<bool> EnableLoopCount("enable-loop-count", cl::Hidden,
-                                        cl::desc("Enable callee count anslysis"),
+static cl::opt<bool> DisableLoopCount("disable-loop-count", cl::Hidden,
+                                        cl::desc("Disable callee count anslysis"),
                                         cl::init(false));
 
-static cl::opt<bool> EnableBBCount("enable-BB-count", cl::Hidden,
-                                        cl::desc("Enable callee count anslysis"),
+static cl::opt<bool> DisableBBCount("disable-BB-count", cl::Hidden,
+                                        cl::desc("Disable callee count anslysis"),
                                         cl::init(false));
 
-static cl::opt<bool> EnableFunctionName("enable-function-name", cl::Hidden,
-                                        cl::desc("Enable callee count anslysis"),
+static cl::opt<bool> DisableFunctionName("disable-function-name", cl::Hidden,
+                                        cl::desc("Disable callee count anslysis"),
                                         cl::init(false));
 
 class FunctionAnalysisBase {
@@ -169,6 +169,7 @@ private:
     StringBuffer strbuf;
     SMDiagnostic Err;
     std::unique_ptr<Module> M;
+    std::vector<FunctionAnalysisBase*> analysisPassManager;
     PrettyWriter<StringBuffer> *writer;
 
 public:
@@ -179,6 +180,10 @@ public:
         }
         writer = new PrettyWriter<StringBuffer>(strbuf);
         return true;
+    }
+
+    void addPass(FunctionAnalysisBase* analysisPass) {
+        analysisPassManager.push_back(analysisPass);
     }
 
     void startFile() {
@@ -194,31 +199,10 @@ public:
         startFile();
         for (Function &F : *M) {
             if (!F.isIntrinsic()) {
-                // get function name 
-                FunctionAnalysisBase* Analysis = new FunctionNamePass();
-                writer->Key("function_name");
-                writer->String(F.getName().str().c_str());
-
-                // get called function
-                Analysis = new FunctionCallPass();
-                Analysis->run(F);
-                Analysis->getResult(*writer);
-
-                // get selection_num
-                Analysis = new SelectionCountPass();
-                Analysis->run(F);
-                Analysis->getResult(*writer);
-
-                // get repetition_num
-                Analysis = new LoopCountPass();
-                Analysis->run(F);
-                Analysis->getResult(*writer);
-
-                // get sequence_num
-                Analysis = new BasicBlockCountPass();
-                Analysis->run(F);
-                Analysis->getResult(*writer);
-                
+                for (auto pass : analysisPassManager) {
+                    pass->run(F);
+                    pass->getResult(*writer);
+                }
             }
         }
         endFile();
@@ -244,6 +228,26 @@ int main(int argc, char **argv) {
     if (!moduleAnalysis.init()) {
         outs() << "Error: Fail to open ir file! \n";
         return 1;
+    }
+
+    if (!DisableFunctionName) {
+        moduleAnalysis.addPass(new FunctionNamePass());
+    }
+
+    if (!DisableCalleeCount) {
+        moduleAnalysis.addPass(new FunctionCallPass());
+    }
+
+    if (!DisableBranchCount) {
+        moduleAnalysis.addPass(new SelectionCountPass());
+    }
+
+    if (!DisableLoopCount) {
+        moduleAnalysis.addPass(new LoopCountPass());
+    }
+
+    if (!DisableBBCount) {
+        moduleAnalysis.addPass(new BasicBlockCountPass());
     }
         
     moduleAnalysis.analyze();
